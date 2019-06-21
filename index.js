c411d20f20ca67619caf162561a42d44755f0c63
Dmitrylyk <dilykmail@gmail.com>
2019-06-21 12:55:12 +0300

    env var test commit

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const BOT_TOKEN = process.env.BOT_TOKEN || '779430236:AAEvVqlYow902TPP1nPzGaecCM
 const bot = new Telegraf('779430236:AAEvVqlYow902TPP1nPzGaecCM3WMRjUYp8');
 bot.start((ctx) => ctx.reply('Welcome! Just send me the name of the group, whose schedule you want to get.'));
 bot.help((ctx) => ctx.reply('There is nothing difficult. Just send me the name of the group, whose schedule you want to get.'));
-bot.on('message', (ctx) => ctx.reply('Hi, friend!'));
 
+bot.launch();
 
-bot.telegram.setWebhook('https://scrapper.dmitrylyk.now.sh');
+//bot.telegram.setWebhook('https://scrapper.dmitrylyk.now.sh');
 
-module.exports = bot.webhookCallback('/');
+//module.exports = bot.webhookCallback('/');
