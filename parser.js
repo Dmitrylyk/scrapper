4b8eb91b4df14722ba52efdcd76bb9814e2f9f72
Dmitrylyk <dilykmail@gmail.com>
2019-06-22 01:14:35 +0300

    Added func of geting URL for separate group

--- parser.js
+++ parser.js
@@ -7,9 +7,33 @@ const URL = 'http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx';
 
 
 const start = function start(groupID) {
-  return getGroupUrl(URL, groupID).then(groupURL => {
+  return getGroupURL(URL, groupID).then(groupURL => {
     return parse(groupURL)
   },
   error => throw (error);
   );
+};
+
+const getGroupURL = function getGroupURL(groupID) {
+  return JSDOM.fromURL(URL).then(dom => {
+    const document = dom.window.document;
+    const formElement = document.getElementById('aspnetForm');
+    const hiddenInputs = formElement.querySelectorAll('input[type="hidden"]');
+
+    const form = {
+      ctl00$MainContent$ctl00$txtboxGroup: group,
+      ctl00$MainContent$ctl00$btnShowSchedule: "Розклад занять"
+    };
+
+    [...hiddenInputs].forEach(elem => { elem.value ? form[elem.name] = elem.value });
+
+    return new Promise(resolve => {
+      request.post({
+        url: URL,
+        form: form
+      },
+      (err, res) => resolve(`http://rozklad.kpi.ua${res.headers.location}`);
+      );
+    });
+  });
 };
