b10dd92b3e3be9c85ec1d35e34cce85ac4ace255
Dmitrylyk <dilykmail@gmail.com>
2019-06-21 13:05:31 +0300

    parser init

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 const Telegraf = require('telegraf');
 
-const BOT_TOKEN = process.env.BOT_TOKEN || '779430236:AAEvVqlYow902TPP1nPzGaecCM3WMRjUYp8';
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const bot = new Telegraf('779430236:AAEvVqlYow902TPP1nPzGaecCM3WMRjUYp8');
+const bot = new Telegraf(BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome! Just send me the name of the group, whose schedule you want to get.'));
 bot.help((ctx) => ctx.reply('There is nothing difficult. Just send me the name of the group, whose schedule you want to get.'));
 
