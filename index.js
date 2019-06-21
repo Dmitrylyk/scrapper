e375f3d3c356d395f49c59485476f3e3e7dfd4b9
Dmitrylyk <dilykmail@gmail.com>
2019-06-21 10:16:57 +0300

    minor fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Telegraf = require('telegraf');
 
 const bot = new Telegraf("process.env.BOT_TOKEN");
-bot.start((ctx) => ctx.reply('Welcome! Just send me the name of the group whose schedule you want to get.'));
-bot.help((ctx) => ctx.reply('There is nothing difficult. Just send me the name of the group whose schedule you want to get.'));
+bot.start((ctx) => ctx.reply('Welcome! Just send me the name of the group, whose schedule you want to get.'));
+bot.help((ctx) => ctx.reply('There is nothing difficult. Just send me the name of the group, whose schedule you want to get.'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
 
