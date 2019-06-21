6be94032389d7cc6e2a1a5d35bab6a6d92d9bd08
Dmitrylyk <dilykmail@gmail.com>
2019-06-21 22:41:12 +0300

    Added input format

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 const bot = new Telegraf(BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome! Just send me the name of the group, whose schedule you want to get.'));
 bot.help((ctx) => ctx.reply('There is nothing difficult. Just send me the name of the group, whose schedule you want to get.'));
-
+bot.hears(/^[А-ЯІа-яі]{2}-[1-9а-яі]{2,5}$/, (ctx) => parse(ctx.message.text)
+    .then(result=>{ctx.reply(result)})
+    .catch(()=>ctx.reply('Wrong input. Try again.')));
 
 //bot.telegram.setWebhook('https://scrapper.dmitrylyk.now.sh');
 
