3159c227992f05005a1506bcbdce2a69abeccf90
Dmitrylyk <dilykmail@gmail.com>
2019-06-21 11:58:44 +0300

    test bot #2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Telegraf = require('telegraf');
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '779430236:AAEvVqlYow902TPP1nPzGaecCM3WMRjUYp8';
 
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf('779430236:AAEvVqlYow902TPP1nPzGaecCM3WMRjUYp8');
 bot.start((ctx) => ctx.reply('Welcome! Just send me the name of the group, whose schedule you want to get.'));
 bot.help((ctx) => ctx.reply('There is nothing difficult. Just send me the name of the group, whose schedule you want to get.'));
 bot.on('message', (ctx) => ctx.reply('Hi, friend!'));
