20881dda6754facd338788c8811b6da7a5dfae4f
Dmitrylyk <dilykmail@gmail.com>
2019-06-22 04:26:06 +0300

    parse and format data

--- parser.js
+++ parser.js
@@ -33,58 +33,30 @@ function getRowData (document) {
 
 function parse(url) {
   return JSDOM.fromURL(url).then(dom => {
-
   const document = dom.window.document;
-  const first = getWeekData('ctl00_MainContent_FirstScheduleTable', document);
-  const second = getWeekData('ctl00_MainContent_SecondScheduleTable', document);
+  const first = dom.window.document.getElementById('ctl00_MainContent_FirstScheduleTable');
+  const second = dom.window.document.getElementById('ctl00_MainContent_SecondScheduleTable');
   const week = {
-    1: '**monday**',
-    2: '**tuesday**',
+    1: 'monday',
+    2: 'tuesday',
     3: 'wednesday',
     4: 'thursday',
     5: 'friday',
     6: 'saturday'
   };
 
+  const firstByTr = first.querySelectorAll('tr');
+  const secondByTr = second.querySelectorAll('tr');
+
+
+
   let firstWeek = {};
   let secondWeek = {};
-  Object.keys(first).forEach(row => {
-  if (row>0) {
-    const firstRow = getRowData(first[row]);
-    const secondRow = getRowData(second[row]);
 
-    Object.keys(week).forEach(day => {
-    if (firstWeek[week[day]] === undefined) {
-      firstWeek[week[day]] = [];
-      secondWeek[week[day]] = [];
-    }
-    try {
-      let temp = {};
-      const firstLength = firstRow[day].getElementsByTagName('a').length;
-      const secondLength = secondRow[day].getElementsByTagName('a').length;
-
-      temp.number = row;
-      temp.name = firstRow[day].getElementsByTagName('a')[0].innerHTML;
-      temp.teacher = firstRow[day].getElementsByTagName('a')[1].innerHTML;
-      temp.classroom = firstRow[day].getElementsByTagName('a')[firstLength-1].innerHTML;
-      firstWeek[week[day]].push(temp);
-
-      temp.name = secondRow[day].getElementsByTagName('a')[0].innerHTML;
-      temp.teacher = secondRow[day].getElementsByTagName('a')[1].innerHTML;
-      temp.classroom = secondRow[day].getElementsByTagName('a')[secondLength-1].innerHTML;
-      secondWeek[week[day]].push(temp);
-    }
-    catch(err) {}
-    });
-  }
-  });
-  const result = '=================\n**FIRST WEEK**\n=================\n\n' +formatData(firstWeek)+
-                 '\n=================\n**SECOND WEEK**\n=================\n\n' + formatData(secondWeek);
-  return new Promise(resolve => {
-    resolve(result);
-  })
+  
   })
 }
+
 
 function formatData(data) {
   let result = '';
