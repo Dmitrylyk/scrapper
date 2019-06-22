857877c0b99ad77c84461230a6026412e0b371af
Dmitrylyk <dilykmail@gmail.com>
2019-06-22 03:12:30 +0300

    functionality testing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const Telegraf = require('telegraf');
-const {parse} = require('./parser.js');
+const {start} = require('./parser.js');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const WEBHOOK_PATH = process.env.WEBHOOK_PATH;
@@ -9,7 +9,7 @@ const WEBHOOK_PATH = process.env.WEBHOOK_PATH;
 const bot = new Telegraf(BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome! Just send me the name of the group, whose schedule you want to get.'));
 bot.help((ctx) => ctx.reply('There is nothing difficult. Just send me the name of the group, whose schedule you want to get.'));
-bot.hears(/^[А-ЯІа-яі]{2}-[1-9а-яі]{2,5}$/, (ctx) => parse(ctx.message.text)
+bot.hears(/^[А-ЯІа-яі]{2}-[1-9а-яі]{2,5}$/, (ctx) => start(ctx.message.text)
     .then(result => {ctx.reply(result)})
     .catch(() => ctx.reply('Schedule for this group was not found. Please, try again.')));
 
